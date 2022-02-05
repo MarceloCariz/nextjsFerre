2c35d7e520506697f12061d2677cf384625712d9
Marcelo <marcelocariz4@gmail.com>
2022-02-05 16:16:23 -0300

    remove sitemap

--- pages/_document.js
+++ pages/_document.js
@@ -1,30 +1,36 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-
-    render() {
-
-        return (
-            <Html lang="es" >
-                <Head />
-                <meta name="description" content="Ferreteria portales, Aridos, Materiales, nos encargamos de atender a todos nuestro clientes
-con el fin de entregarles una solucion. Nos ubicamos en Av diego portales 4497" />
-                <meta property='og:title' content='Ferreteria Portales | Diego Portales | Puente Alto' />
-                <meta name="title" content="Ferreteria Portales | Diego Portales | Puente Alto" />
-                <meta property="og:url" content="http://www.ferreteriasportales.com/" />
-                <link rel="canonical" href="https://www.ferreteriasportales.com/" />
-                <meta property="og:site_name" content="Ferretería Portales" />
-                <meta property="og:type" content="website" />
-                <meta name="robots" content="index, follow" />
-                <title>Ferreteria Portales   Puente Alto  Av Diego Portales</title>
-                <body>
-                <Main />
+  render() {
+    return (
+      <Html lang="es">
+        <Head />
+        <meta
+          name="description"
+          content="Ferreteria portales, Aridos, Materiales, nos encargamos de atender a todos nuestro clientes
+con el fin de entregarles una solucion. Nos ubicamos en Av diego portales 4497"
+        />
+        <meta
+          property="og:title"
+          content="Ferreteria Portales | Diego Portales | Puente Alto"
+        />
+        <meta
+          name="title"
+          content="Ferreteria Portales | Diego Portales | Puente Alto"
+        />
+        <meta property="og:url" content="http://www.ferreteriasportales.com/" />
+        <link rel="canonical" href="https://www.ferreteriasportales.com/" />
+        <meta property="og:site_name" content="Ferretería Portales" />
+        <meta property="og:type" content="website" />
+        <meta name="robots" content="index, follow" />
+        <title>Ferreteria Portales Puente Alto Av Diego Portales</title>
+        <body>
+          <Main />
           <NextScript />
-
-                </body>
-            </Html>)
-    }
-
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
+export default MyDocument;
