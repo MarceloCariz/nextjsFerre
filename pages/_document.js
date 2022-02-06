7bea90b1bed20a903f2f8d69fd8c95ebd5e1202f
Marcelo <marcelocariz4@gmail.com>
2022-02-06 16:52:55 -0300

    index, nofllow

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ con el fin de entregarles una solucion. Nos ubicamos en Av diego portales 4497" 
                 <link rel="canonical" href="http://www.ferreteriasportales.com/" />
                 <meta property="og:site_name" content="Ferretería Portales" />
                 <meta property="og:type" content="website" />
-                <meta name="robots" content="index, follow" />
+                <meta name="robots" content="index, nofollow" />
                 <title>Ferreteria Portales   Puente Alto  Av Diego Portales</title>
                 <body>
                 <Main />
